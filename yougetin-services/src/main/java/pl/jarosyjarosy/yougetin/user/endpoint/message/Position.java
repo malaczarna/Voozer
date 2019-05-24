ccff330bb461becdbce766be731a3fb5f9f6fb1d
jarosy <jarosyjarosy@gmail.com>
2019-05-24 18:12:37 +0200

    pozycje uÅytkownikow, aktualny profil, nowe endpointy pod nowe pola

---
+++ yougetin-services/src/main/java/pl/jarosyjarosy/yougetin/user/endpoint/message/Position.java
@@ -0,0 +1,22 @@
+package pl.jarosyjarosy.yougetin.user.endpoint.message;
+
+public class Position {
+    private Double Lat;
+    private Double Lng;
+
+    public Double getLat() {
+        return Lat;
+    }
+
+    public void setLat(Double lat) {
+        Lat = lat;
+    }
+
+    public Double getLng() {
+        return Lng;
+    }
+
+    public void setLng(Double lng) {
+        Lng = lng;
+    }
+}
--- yougetin-services/src/main/java/pl/jarosyjarosy/yougetin/user/repository/UserRepository.java
+++ yougetin-services/src/main/java/pl/jarosyjarosy/yougetin/user/repository/UserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import pl.jarosyjarosy.yougetin.user.model.Profile;
 import pl.jarosyjarosy.yougetin.user.model.User;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Qualifier("users")
 @Repository
@@ -15,4 +17,8 @@ import javax.transaction.Transactional;
 public interface UserRepository extends CrudRepository<User, Long>, QueryByExampleExecutor<User> {
 
     User findByEmail(String email);
+
+    List<User> findAllByCurrentProfile(Profile profile);
+
+
 }
